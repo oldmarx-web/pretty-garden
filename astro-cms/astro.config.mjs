a31d3f7e56bab363d99656ec4d9e1dd86f256bbf
oldmarx-web <oldmarx@gmail.com>
2026-05-12 17:09:38 +0900

    Delete astro-cms directory

--- astro-cms/astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({
-  // 배포 후 사이트 주소로 바꿔주세요 (예: 'https://prettygarden.kr')
-  site: 'https://prettygarden.netlify.app',
-});
